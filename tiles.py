38419ca0baa0c53200e2f8f23b33e0e66c62abe2
CEASLIBRARY <30638279+CEASLIBRARY@users.noreply.github.com>
2022-08-26 16:45:40 -0400

    Update tiles.py

--- tiles.py
+++ tiles.py
@@ -1,11 +1,11 @@
 import pygame 
 
 class Tile(pygame.sprite.Sprite):
-	def __init__(self,size,x,y):
-		super().__init__()
-		self.image = pygame.Surface((size,size))
-		self.image.fill('grey')
-		self.rect = self.image.get_rect(topleft = (x,y))
+    def __init__(self,size,x,y):
+        super().__init__()
+        self.image = pygame.Surface((size,size))
+        self.image.fill('grey')
+        self.rect = self.image.get_rect(topleft = (x,y))
 
-	def update(self,shift):
-		self.rect.x += shift
+    def update(self,shift):
+        self.rect.x += shift
