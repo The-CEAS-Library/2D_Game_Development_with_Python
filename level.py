e6e0ba0980d3c0fab07410fb9c8c3ccaf9920693
CEASLIBRARY <30638279+CEASLIBRARY@users.noreply.github.com>
2022-08-26 16:39:24 -0400

    Update level.py

--- level.py
+++ level.py
@@ -6,11 +6,11 @@ from player import Player
 class Level:
 	def __init__(self,level_data,surface):
 
-			# level set up
-			self.display_surface = surface
-			self.setup_level(level_data)
+		# level set up
+		self.display_surface = surface
+		self.setup_level(level_data)
 
-			self.world_shift = 0
+		self.world_shift = 0
 
 	def setup_level(self, layout):
 
@@ -22,33 +22,33 @@ class Level:
 			for col_index, cell in enumerate(row):
 
 				x = col_index * tile_size
-					y = row_index * tile_size
+				y = row_index * tile_size
 
-					if cell = 'X':
-						tile = Tile((x,y), tile_size)                                        
-						# x,y must be a tuple!
-						self.tiles.add(tile)
+				if cell = 'X':
+					tile = Tile((x,y), tile_size)                                        
+					# x,y must be a tuple!
+					self.tiles.add(tile)
 
-					if cell = 'P':
-						player_sprite = Player((x,y))
-						self.player.add(player_sprite)
+				if cell = 'P':
+					player_sprite = Player((x,y))
+					self.player.add(player_sprite)
 
 	def scroll_x(self):
-			player = self.player.sprite
-			player_x = player.rect.centerx
-			direction_x = player.direction.x
+		player = self.player.sprite
+		player_x = player.rect.centerx
+		direction_x = player.direction.x
 
-			#left of the screen
-			if player_x < screen_width/4 and direction_x < 0:
-					self.world_shift = 8
-					player.speed = 0
-			elif player_x > screen_width - (screen_width/4) and direction_x > 0:
-					self.world_shift = -8
-					player.speed = 0
+		#left of the screen
+		if player_x < screen_width/4 and direction_x < 0:
+				self.world_shift = 8
+				player.speed = 0
+		elif player_x > screen_width - (screen_width/4) and direction_x > 0:
+				self.world_shift = -8
+				player.speed = 0
 
-			else: # to make the level scroll along with the player
-					self.world_shift = 0
-					player.speed = 8
+		else: # to make the level scroll along with the player
+				self.world_shift = 0
+				player.speed = 8
 
 
         
