fa8e16e56a8f35fd45108605df152b587605cb27
CEASLIBRARY <30638279+CEASLIBRARY@users.noreply.github.com>
2022-08-26 16:40:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame, sys
 from settings import * 
 from level import Level
 
-# Pygame setup
+# Pygame Setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
