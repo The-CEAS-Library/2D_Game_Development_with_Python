ab202080b238a60b576d22a9e3eb9eec559c2a2d
CEASLIBRARY <30638279+CEASLIBRARY@users.noreply.github.com>
2022-08-27 18:37:09 +0530

    Update player.py

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
 
         self.image = pygame.Surface((32,64))
-        self.image.fill('red)
+        self.image.fill('red')
         self.rect = self.image.get_rect(topleft = pos)
 
         # VECTORS
