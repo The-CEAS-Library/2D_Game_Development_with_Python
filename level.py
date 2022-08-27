0c5b4a673793bc7748b27a0aeea82718aff500ed
CEASLIBRARY <30638279+CEASLIBRARY@users.noreply.github.com>
2022-08-27 16:14:14 +0530

    Update level.py

--- level.py
+++ level.py
@@ -49,6 +49,7 @@ class Level:
         if player_x < screen_width/4 and direction_x < 0:
                 self.world_shift = 8
                 player.speed = 0
+                
         elif player_x > screen_width - (screen_width/4) and direction_x > 0:
                 self.world_shift = -8
                 player.speed = 0
